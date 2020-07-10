af2b42156bda81a5e72a6a1bdbbe6ed6d647af4a
Samihan Deshmukh <samihan25@gmail.com>
2020-07-10 15:28:15 +0530

    added comments

--- Chrome_game_random_bot.py
+++ Chrome_game_random_bot.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import os
-from decouple import config
-from selenium.webdriver.common.action_chains import ActionChains
 
+#creating object of Firefox driver
 browser = webdriver.Firefox()
 
+#open website which have chrome T-Rex dino game
 browser.get('https://chromedino.com/')
 time.sleep(1)
-#assert 'Google' in browser.title
-# time.sleep(2)
+
+#select some element
 elem = browser.find_element_by_id('t')
+
+#outer loop for every new game
 while True:
+    #inner loop for each step in a game
     while True:
         elem.send_keys(Keys.SPACE)
-        time.sleep(0.3)
-    time.sleep(1)
+        time.sleep(0.3)             # Time gap of 30 miliseconds is given between each move
+    time.sleep(1)                   # Time gap of 1 second between each different game
